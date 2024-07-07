ea6a20c0498da954ee3ca8aa27fdada88e22dca8
Sebastian <shuertas981@gmail.com>
2024-07-07 03:57:49 -0500

    Mensaje exitoso de compra de butacas y recuperacion de contraseña

--- app/src/main/java/com/cinespoiler/EntradasFragment.kt
+++ app/src/main/java/com/cinespoiler/EntradasFragment.kt
@@ -1,20 +1,20 @@
 package com.cinespoiler
 
 import android.app.DatePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-
-import com.bumptech.glide.Glide
-import com.cinespoiler.adapter.MovieAdapter
-import com.cinespoiler.model.Movie
-import com.cinespoiler.ui.fragments.MovieDetailsFragment
+import com.cinespoiler.ui.fragments.MensajeExitosCompraFragment
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -27,6 +27,9 @@ class EntradasFragment : Fragment() {
     private lateinit var moviePrice: TextView
     private lateinit var tvTotalPrice: TextView
     private lateinit var fechaCompra: EditText
+    private lateinit var btnPagar: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
     private var seleccionarButacas = mutableListOf<String>()
     private var totalPrice = 0.0
     private var butacaPrice = 0.0
@@ -43,6 +46,8 @@ class EntradasFragment : Fragment() {
         fechaCompra = view.findViewById(R.id.etFechaCompra)
         tvButacaTotal = view.findViewById(R.id.tvButacaTotal)
         moviePrice = view.findViewById(R.id.tvMoviePriceButaca)
+        btnPagar = view.findViewById(R.id.btnPagar)
+        sharedPreferences = requireActivity().getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
          btnButaca = listOf(
                 view.findViewById(R.id.A1),
                 view.findViewById(R.id.A2),
@@ -67,8 +72,14 @@ class EntradasFragment : Fragment() {
         fechaCompra.setOnClickListener { mostrarDatePicker() }
 
         setupAsientoButacas(view)
+        btnPagar.setOnClickListener {
+            completarMensaje()
+        }
+
         return view
     }
+
+
 
     private fun setupAsientoButacas(view: View?) {
         for(button in btnButaca){
@@ -79,12 +90,12 @@ class EntradasFragment : Fragment() {
     }
 
     private fun toggleSeleccionarButacas(button: ImageButton) {
-        val butacasId = button.id.toString()
-        if(seleccionarButacas.contains(butacasId)){
-            seleccionarButacas.remove(butacasId)
+        val butacasTag = button.tag.toString()
+        if(seleccionarButacas.contains(butacasTag)){
+            seleccionarButacas.remove(butacasTag)
             totalPrice -= butacaPrice
         }else {
-            seleccionarButacas.add(butacasId)
+            seleccionarButacas.add(butacasTag)
             totalPrice += butacaPrice
         }
         tvTotalPrice.text = getString(R.string.total_price_format, totalPrice)
@@ -93,19 +104,24 @@ class EntradasFragment : Fragment() {
 
     private fun mostrarDatePicker() {
         val calendar = Calendar.getInstance()
+        val minDate = calendar.timeInMillis
+        calendar.add(Calendar.DAY_OF_MONTH, 5)
+        val maxDate = calendar.timeInMillis
+
         val datePickerDialog = DatePickerDialog(
             requireContext(),
             { _, year, month, dayOfMonth ->
                 val date = Calendar.getInstance()
                 date.set(year, month, dayOfMonth)
-                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
                 fechaCompra.setText(dateFormat.format(date.time))
             },
             calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH),
             calendar.get(Calendar.DAY_OF_MONTH)
         )
-        datePickerDialog.datePicker.minDate = calendar.timeInMillis
+        datePickerDialog.datePicker.minDate = minDate
+        datePickerDialog.datePicker.maxDate = maxDate
         datePickerDialog.show()
     }
 
@@ -128,8 +144,23 @@ class EntradasFragment : Fragment() {
                     butacaPrice = price
                 }
             }.addOnFailureListener{
-                Log.e("Cargar detalle pelicula", "Ha ocurrido un error al obterner los datoos")
+                Log.e("Cargar detalle pelicula", "Ha ocurrido un error al obterner los datos")
             }
+    }
+    private fun viewMsgExitoCompra(){
+        auth= FirebaseAuth.getInstance()
+        val userId = auth.currentUser?.uid ?: "N/A"
+        val userName = sharedPreferences.getString("user_name", "N/A") ?: "N/A"
+        val movieNameText = movieName.text.toString()
+        val functionDateText = fechaCompra.text.toString()
+        val montoTotal = totalPrice
+        val butacasText = seleccionarButacas.joinToString(", ")
+
+        val dialog = MensajeExitosCompraFragment.newInstance(userName, movieNameText, functionDateText, montoTotal, butacasText)
+        dialog.show(parentFragmentManager, "MensajeExitosCompraFragment")
+    }
+    private fun completarMensaje() {
+       viewMsgExitoCompra()
     }
     companion object{
         fun newInstance(movieId: String): EntradasFragment {
